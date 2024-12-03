b3a39d14ac5795f16fd9eed22544e9518403ddeb
JUlianCVM <111820807+JulianCVM@users.noreply.github.com>
2024-12-03 07:30:12 -0500

    #Se actualiza el .py

--- Python/Variables.py
+++ Python/Variables.py
@@ -1,3 +1,20 @@
 name = "Julian V"
-age  = 24
-heigth = 1.63
+age  = 19
+heigth = 1.73
+teacher  = False
+trainer = True
+#Conjunto de datos
+hobbies = list()
+hobbies = ["Cocinar", "Programar", "Jugar", "Dormir", "Musica"]
+
+#print(name, age, heigth, teacher, trainer, hobbies)
+print(hobbies[2])
+print(len(hobbies))
+
+hobbies[2]="Despertarme"
+print(hobbies)
+
+
+dato = input("Ingrese otro pasatiempo:")
+
+hobbies.append(dato)
