3414af861fde7f6db0cfe49df9870de1c38f28e5
JUlianCVM <111820807+JulianCVM@users.noreply.github.com>
2024-12-03 07:48:45 -0500

    #Se agregan comentarios

--- Python/Variables.py
+++ Python/Variables.py
@@ -1,3 +1,5 @@
+#Tipos de datos primitivos
+#String, int, char, float, bool
 name = "Julian V"
 age  = 19
 heigth = 1.73
@@ -8,13 +10,18 @@ hobbies = list()
 hobbies = ["Cocinar", "Programar", "Jugar", "Dormir", "Musica"]
 
 #print(name, age, heigth, teacher, trainer, hobbies)
+#Se imprime la lista
 print(hobbies[2])
 print(len(hobbies))
-
+#Se asigna un valor a un espacio en la lista, se cambia el valor
 hobbies[2]="Despertarme"
 print(hobbies)
 
-
+#Se solicita un dato
 dato = input("Ingrese otro pasatiempo:")
-
+#Se ingresa un dato
 hobbies.append(dato)
+#Se elimina un dato
+hobbies.pop(2)
+print(hobbies)
+
