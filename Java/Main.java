2da681662953796396c5d87d80670d737afe885d
JUlianCVM <111820807+JulianCVM@users.noreply.github.com>
2024-12-03 07:29:23 -0500

    #Se agrega un ejercicio simple de java

---
+++ Java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String [] args){
+        int a = 2;
+        int b = 3;
+        int c = 1;
+        
+        object obj = new object(a, b, c);
+        
+        System.out.println(obj.Suma(a, b, c));
+        
+    }
+}
---
+++ Java/object.java
@@ -0,0 +1,14 @@
+public class object{
+    int a, b, c;
+    
+    public object(int a,int b, int c){
+        this.a = a;
+        this.b  = b;
+        this.c = c;
+    }
+
+    int Suma(int a,int b,int c){
+        int r;
+        return r = a + b + c;
+    }
+}
